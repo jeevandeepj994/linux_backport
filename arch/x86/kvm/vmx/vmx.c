60a023887cde00f192b747d310cc95194740389f
Sean Christopherson <seanjc@google.com>
2023-08-24 18:36:20 -0700

    KVM: x86: Refactor can_emulate_instruction() return to be more expressive
    commit aeb904f6b9f1de588cf3130dc8a2c458b236704e upstream.
    
    Refactor and rename can_emulate_instruction() to allow vendor code to
    return more than true/false, e.g. to explicitly differentiate between
    "retry", "fault", and "unhandleable".  For now, just do the plumbing, a
    future patch will expand SVM's implementation to signal outright failure
    if KVM attempts EMULTYPE_SKIP on an SEV guest.
    
    No functional change intended (or rather, none that are visible to the
    guest or userspace).
    
    Link: https://lore.kernel.org/r/20230825013621.2845700-4-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: suryasaimadhu <saimadhu.koyyalaharivenkata@amd.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -1670,8 +1670,8 @@ static int vmx_rtit_ctl_check(struct kvm_vcpu *vcpu, u64 data)
 	return 0;
 }
 
-static bool vmx_can_emulate_instruction(struct kvm_vcpu *vcpu, int emul_type,
-					void *insn, int insn_len)
+static int vmx_check_emulate_instruction(struct kvm_vcpu *vcpu, int emul_type,
+					 void *insn, int insn_len)
 {
 	/*
 	 * Emulation of instructions in SGX enclaves is impossible as RIP does
@@ -1682,9 +1682,9 @@ static bool vmx_can_emulate_instruction(struct kvm_vcpu *vcpu, int emul_type,
 	 */
 	if (to_vmx(vcpu)->exit_reason.enclave_mode) {
 		kvm_queue_exception(vcpu, UD_VECTOR);
-		return false;
+		return X86EMUL_PROPAGATE_FAULT;
 	}
-	return true;
+	return X86EMUL_CONTINUE;
 }
 
 static int skip_emulated_instruction(struct kvm_vcpu *vcpu)
@@ -5816,7 +5816,7 @@ static int handle_ept_misconfig(struct kvm_vcpu *vcpu)
 {
 	gpa_t gpa;
 
-	if (!vmx_can_emulate_instruction(vcpu, EMULTYPE_PF, NULL, 0))
+	if (vmx_check_emulate_instruction(vcpu, EMULTYPE_PF, NULL, 0))
 		return 1;
 
 	/*
@@ -8442,7 +8442,7 @@ static struct kvm_x86_ops vmx_x86_ops __initdata = {
 	.enable_smi_window = vmx_enable_smi_window,
 #endif
 
-	.can_emulate_instruction = vmx_can_emulate_instruction,
+	.check_emulate_instruction = vmx_check_emulate_instruction,
 	.apic_init_signal_blocked = vmx_apic_init_signal_blocked,
 	.migrate_timers = vmx_migrate_timers,
 
