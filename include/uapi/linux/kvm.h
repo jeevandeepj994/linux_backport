13c3fad87579be16e29ec8060ad54bf9b96981a1
Chao Peng <chao.p.peng@linux.intel.com>
2023-10-27 11:21:55 -0700

    KVM: Introduce per-page memory attributes
    commit 5a475554db1e476a14216e742ea2bdb77362d5d5 upstream
    
    In confidential computing usages, whether a page is private or shared is
    necessary information for KVM to perform operations like page fault
    handling, page zapping etc. There are other potential use cases for
    per-page memory attributes, e.g. to make memory read-only (or no-exec,
    or exec-only, etc.) without having to modify memslots.
    
    Introduce the KVM_SET_MEMORY_ATTRIBUTES ioctl, advertised by
    KVM_CAP_MEMORY_ATTRIBUTES, to allow userspace to set the per-page memory
    attributes to a guest memory range.
    
    Use an xarray to store the per-page attributes internally, with a naive,
    not fully optimized implementation, i.e. prioritize correctness over
    performance for the initial implementation.
    
    Use bit 3 for the PRIVATE attribute so that KVM can use bits 0-2 for RWX
    attributes/protections in the future, e.g. to give userspace fine-grained
    control over read, write, and execute protections for guest memory.
    
    Provide arch hooks for handling attribute changes before and after common
    code sets the new attributes, e.g. x86 will use the "pre" hook to zap all
    relevant mappings, and the "post" hook to track whether or not hugepages
    can be used to map the range.
    
    To simplify the implementation wrap the entire sequence with
    kvm_mmu_invalidate_{begin,end}() even though the operation isn't strictly
    guaranteed to be an invalidation.  For the initial use case, x86 *will*
    always invalidate memory, and preventing arch code from creating new
    mappings while the attributes are in flux makes it much easier to reason
    about the correctness of consuming attributes.
    
    It's possible that future usages may not require an invalidation, e.g.
    if KVM ends up supporting RWX protections and userspace grants _more_
    protections, but again opt for simplicity and punt optimizations to
    if/when they are needed.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/all/Y2WB48kD0J4VGynX@google.com
    Cc: Fuad Tabba <tabba@google.com>
    Cc: Xu Yilun <yilun.xu@intel.com>
    Cc: Mickaël Salaün <mic@digikod.net>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20231027182217.3615211-14-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Jeevan deep J <j.jeevandeep@amd.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1220,6 +1220,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_ARM_SUPPORTED_REG_MASK_RANGES 230
 #define KVM_CAP_USER_MEMORY2 231
 #define KVM_CAP_MEMORY_FAULT_INFO 232
+#define KVM_CAP_MEMORY_ATTRIBUTES 233
 
 #define KVM_CAP_SEV_ES_GHCB 500
 
@@ -2378,5 +2379,16 @@ struct kvm_csv_init {
 /* ioctls for control vm during system reset, currently only for CSV */
 #define KVM_CONTROL_PRE_SYSTEM_RESET	 _IO(KVMIO, 0xe8)
 #define KVM_CONTROL_POST_SYSTEM_RESET	 _IO(KVMIO, 0xe9)
+/* Available with KVM_CAP_MEMORY_ATTRIBUTES */
+#define KVM_SET_MEMORY_ATTRIBUTES              _IOW(KVMIO,  0xd2, struct kvm_memory_attributes)
+
+struct kvm_memory_attributes {
+	__u64 address;
+	__u64 size;
+	__u64 attributes;
+	__u64 flags;
+};
+
+#define KVM_MEMORY_ATTRIBUTE_PRIVATE           (1ULL << 3)
 
 #endif /* __LINUX_KVM_H */
