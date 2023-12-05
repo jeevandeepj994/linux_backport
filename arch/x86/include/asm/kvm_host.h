71e4bbf8575973030bc36deb0eff6c3728aba175
Vitaly Kuznetsov <vkuznets@redhat.com>
2023-12-05 11:36:26 +0100

    KVM: x86: Make Hyper-V emulation optional
    commit b4f69df0f65e97fec439130a0d0a8b9c7cc02df2 upstream.
    
    Hyper-V emulation in KVM is a fairly big chunk and in some cases it may be
    desirable to not compile it in to reduce module sizes as well as the attack
    surface. Introduce CONFIG_KVM_HYPERV option to make it possible.
    
    Note, there's room for further nVMX/nSVM code optimizations when
    !CONFIG_KVM_HYPERV, this will be done in follow-up patches.
    
    Reorganize Makefile a bit so all CONFIG_HYPERV and CONFIG_KVM_HYPERV files
    are grouped together.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Reviewed-by: Maxim Levitsky <mlevitsk@redhat.com>
    Tested-by: Jeremi Piotrowski <jpiotrowski@linux.microsoft.com>
    Link: https://lore.kernel.org/r/20231205103630.1391318-13-vkuznets@redhat.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: PrithivishS <Shivdasani.Prithivish@amd.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -931,8 +931,10 @@ struct kvm_vcpu_arch {
 	/* used for guest single stepping over the given code position */
 	unsigned long singlestep_rip;
 
+#ifdef CONFIG_KVM_HYPERV
 	bool hyperv_enabled;
 	struct kvm_vcpu_hv *hyperv;
+#endif
 #ifdef CONFIG_KVM_XEN
 	struct kvm_vcpu_xen xen;
 #endif
@@ -1089,6 +1091,7 @@ enum hv_tsc_page_status {
 	HV_TSC_PAGE_BROKEN,
 };
 
+#ifdef CONFIG_KVM_HYPERV
 /* Hyper-V emulation context */
 struct kvm_hv {
 	struct mutex hv_lock;
@@ -1122,6 +1125,7 @@ struct kvm_hv {
 	struct hv_partition_assist_pg *hv_pa_pg;
 	struct kvm_hv_syndbg hv_syndbg;
 };
+#endif
 
 struct msr_bitmap_range {
 	u32 flags;
@@ -1344,7 +1348,9 @@ struct kvm_arch {
 	/* reads protected by irq_srcu, writes by irq_lock */
 	struct hlist_head mask_notifier_list;
 
+#ifdef CONFIG_KVM_HYPERV
 	struct kvm_hv hyperv;
+#endif
 
 #ifdef CONFIG_KVM_XEN
 	struct kvm_xen xen;
