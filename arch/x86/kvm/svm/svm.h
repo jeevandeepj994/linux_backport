998f26e828ad7f8048b9b9e99575df8cb2b890b8
Brijesh Singh <brijesh.singh@amd.com>
2024-05-01 03:51:58 -0500

    KVM: SEV: Add support to handle GHCB GPA register VMGEXIT
    commit 0c76b1d08280649f789e1b537b397cefc43da7a0 upstream
    
    SEV-SNP guests are required to perform a GHCB GPA registration. Before
    using a GHCB GPA for a vCPU the first time, a guest must register the
    vCPU GHCB GPA. If hypervisor can work with the guest requested GPA then
    it must respond back with the same GPA otherwise return -1.
    
    On VMEXIT, verify that the GHCB GPA matches with the registered value.
    If a mismatch is detected, then abort the guest.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Message-ID: <20240501085210.2213060-9-michael.roth@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -214,6 +214,7 @@ struct vcpu_sev_es_state {
 	/* CSV2 reboot vmsa */
 	struct vmcb_save_area *reset_vmsa;
 #endif
+	u64 ghcb_registered_gpa;
 };
 
 struct vcpu_svm {
@@ -365,6 +366,11 @@ static __always_inline bool sev_snp_guest(struct kvm *kvm)
 #else
 	return false;
 #endif
+}
+
+static inline bool ghcb_gpa_is_registered(struct vcpu_svm *svm, u64 val)
+{
+	return svm->sev_es.ghcb_registered_gpa == val;
 }
 
 static inline void vmcb_mark_all_dirty(struct vmcb *vmcb)
